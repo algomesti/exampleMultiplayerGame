e49e05c328941ee9ed44bfe3bf9ccca8e99c4cf8
Alexandre Gomes <algomes.ti@gmail.com>
2019-12-29 10:57:37 -0300

    version 0.1

---
+++ src/poc/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/game.js'
+import socketio from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const sockets = socketio(server)
+
+app.use(express.static('public'))
+const game = createGame()
+
+game.subscribe((command) => {
+    console.log(`> Emitting  ${command.type}`)
+    sockets.emit(command.type, command)
+})
+/*
+game.unSubscribe((command) => {
+    console.log(`> Emitting  ${command.type}`)
+    sockets.emit(command.type, command)
+})
+*/
+sockets.on('connection', (socket) => {
+    const playerId = socket.id   
+    game.addPlayer({playerId: playerId})
+
+    socket.emit('setup', game.state)
+    
+    socket.on('disconnect', () => {
+        game.removePlayer({playerId: playerId})
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId,
+        command.type = 'move-player'
+        game.movePlayer(command)
+    })
+})
+server.listen(
+    3000,
+    () => {
+        console.log('server running on port 3000')   
+    }
+)
